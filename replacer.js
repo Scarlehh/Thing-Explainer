b88a45faf30f576fb1392139d05246690f70750a
Scarlett <gourleys@tcd.ie>
2016-04-02 15:19:06 +0100

    Added a thing and it works now?!

--- replacer.js
+++ replacer.js
@@ -1,26 +1,28 @@
-var elements = document.getElementsByTagName('*');
+function replacer() {
+	var elements = document.getElementsByTagName('*');
 
-var wordPairs = {
-   cat: "dog",
-   dog: "hitler",
-};
+	for (var i = 0; i < elements.length; i++) {
+		var element = elements[i];
 
-function explainify(word){
-	return wordPairs[word];
+		for (var j = 0; j < element.childNodes.length; j++) {
+		    var node = element.childNodes[j];
+
+		    if (node.nodeType === 3) {
+		        var text = node.nodeValue;
+		        var replacedText = text.replace(/Hitler/gi, 'Hatler');
+
+		        if (replacedText !== text) {
+		            element.replaceChild(document.createTextNode(replacedText), node);
+		        }
+		    }
+		}
+	}
 }
 
-for (var i = 0; i < elements.length; i++) {
-    var element = elements[i];
-
-    for (var j = 0; j < element.childNodes.length; j++) {
-        var node = element.childNodes[j];
-
-        if (node.nodeType === 3) {
-            var text = node.nodeValue;
-			var str = text.replace(/cat|dog|goat/gi, explainify);
-            if (str !== text) {
-                element.replaceChild(document.createTextNode(str), node);
-            }
+chrome.runtime.onMessage.addListener(
+    function(message, sender, sendResponse) {
+        if (message.command === "highlight"){
+            replacer();
         }
     }
-}
+);
