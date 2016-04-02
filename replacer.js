51d109e1443d22b2854b2bc5fac7bf8a6ec3050a
Dário Tavares Antunes <dario@ntun.es>
2016-04-02 14:53:24 +0100

    Fixed regex and added euthanasia vocabulary.

--- replacer.js
+++ replacer.js
@@ -3,11 +3,30 @@ var elements = document.getElementsByTagName('*');
 var wordPairs = {
    cat: "dog",
    dog: "hitler",
+   euthanasia: "nice death",
+   disease: "bad form",
+   diseases: "bad forms",
+   suffer: "hurt",
+   regarded: "looked at",
+   assisted: "helped",
+   suicide: "self death",
+   murder: "other person kill",
+   intent: "plan",
+   suffering: "hurting",
+   voluntary: "wanted by the person",
+   specifically: "definitely",
+   non: "not",
+   involuntary: "not wanted by the person",
+   generally: "usually",
+   illegal: "not allowed",
+   country: "area of people with the same tongue",
+   countries: "areas of people with the same tongue",
 };
 
 function explainify(word){
-	var candidate = wordPairs[word.toLowerCase()];
-	return candidate === null? word : candidate;
+	var candidate = wordPairs[word.toLowerCase().trim()];
+	debugger
+	return (candidate === null || candidate === undefined ? word : candidate);
 }
 
 for (var i = 0; i < elements.length; i++) {
@@ -18,7 +37,7 @@ for (var i = 0; i < elements.length; i++) {
 
         if (node.nodeType === 3) {
             var text = node.nodeValue;
-			var str = text.replace(/cat|dog|goat/gi, explainify);
+			var str = text.replace(/\b[a-zA-Z]+?\b/gi, explainify);
             if (str !== text) {
                 element.replaceChild(document.createTextNode(str), node);
             }
