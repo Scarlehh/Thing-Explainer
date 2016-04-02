d4ee89ff656391e9a2430c41ed21f3ca006cb1c4
andrew-shaw <shawa1@tcd.ie>
2016-04-02 14:46:40 +0100

    easter rising

--- replacer.js
+++ replacer.js
@@ -1,9 +1,31 @@
 var elements = document.getElementsByTagName('*');
 
 var wordPairs = {
-   cat: "dog",
-   dog: "hitler",
+	cat: "dog",
+	dog: "hitler",
+	easter: "god son living again day",
+	irish: "green",
+	rebellion: "fighting against",
+	insurrection: "against-ing",
+	ireland: "green place",
+	april: "month four",
+	launched: "fired in the air",
+	republicans: "alone country-running people",
+	british: "other country",
+	rule: "making to to",
+	establish: "make",
+	independent: "on their own",
+	republic: "alone country",
+	united: "together",
+	kingdom: "big man at the top country",
+	heavily: "strongly",
+	engaged: "got into",
+	significant: "really big",
+	uprising: "big fight",
+	action: "doing",
 };
+
+var re = new RegExp(Object.keys(wordPairs).join('|'), 'gi');
 
 function explainify(word){
 	var candidate = wordPairs[word.toLowerCase()];
@@ -18,7 +40,7 @@ for (var i = 0; i < elements.length; i++) {
 
         if (node.nodeType === 3) {
             var text = node.nodeValue;
-			var str = text.replace(/cat|dog|goat/gi, explainify);
+			var str = text.replace(re, explainify);
             if (str !== text) {
                 element.replaceChild(document.createTextNode(str), node);
             }
